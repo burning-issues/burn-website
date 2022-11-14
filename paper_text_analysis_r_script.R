f85f2d9b808f0f0d2124443f4ff6e60a2e21dd3a
Francisco J. Guerrero <97982871+guerrero-fj@users.noreply.github.com>
2022-11-14 14:08:50 -0800

    Updated script for pdf analysis

--- paper_text_analysis_r_script.R
+++ paper_text_analysis_r_script.R
@@ -18,19 +18,40 @@ ppr <- pdf_text("assets/wq_burning1.pdf")
 info <- pdf_info("assets/wq_burning1.pdf")
 ppr_txt <- data.frame(line = 1:8, text = ppr)
 
+a <- as.vector(unlist(strsplit(info$keys$Subject," ")))
+
+ppr_cln %>% ppr_txt %>% 
+  sub(paste(a, collapse = '|'),'',ppr_txt$text)
+
+
+ppr_txt$text <- sub(paste(a, collapse = '|'),'',ppr_txt)
+
+
+ppr_cln <- ppr_txt %>% 
+  unnest_tokens(output = word, input = text)
+
+
 subject <- info$keys$Subject
 domain <- info$keys$`CrossMarkDomains[1]`
 creator <- info$keys$Creator
 doi <- info$keys$doi
 cdomain <- info$keys$`CrossMarkDomains[2]`
 
+a <- as.vector(unlist(strsplit(info$keys$Subject," ")))
+b <- strsplit(info$keys$Subject," ")
 
 
 # Cleanning up the file
+ppr_txt1 <- ppr_txt %>% 
+  filter(str_remove_all(text,paste(b,collapse = "|")))
+ 
+ppr_txt2 <- ppr_txt %>%
+  filter(str_detect(text,paste(b,collapse = "|")))
+
 
 ppr_cln <- ppr_txt %>% 
-  unnest_tokens(output = word, input = text) %>% 
-  str_detect(word,paste(subject))
+  # unnest_tokens(output = word, input = text) %>% 
+  filter(str_detect(ppr_txt,paste(a,collapse = "|")),negate = TRUE)
 %>% 
   filter(str_detect(word,paste(domain))) %>% 
   filter(str_detect(word,paste(creator))) %>% 
